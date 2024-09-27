5550c0ef064cd6eb7b00273f5dab71a16444f092
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2024-09-27 14:25:12 -0400

    configs

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HomePage from './pages/home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './index.css'
+import RegisterPage from './pages/register'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
